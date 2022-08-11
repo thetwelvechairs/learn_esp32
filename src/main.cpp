54a32af0e70cf73b1115f62cb2c0fdab8ec4a885
vokamis <asimakov@twelvechairs-software.com>
2022-08-10 21:00:26 -0400

    INIT

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <SPI.h>
+#include <fabgl.h>
+#include <iostream>
+
+#define SD_CS     8   // SD card select pin
+#define TFT_DC    9
+#define TFT_CS    10
+
+#define TFT_RST   -1  // Or set to -1 and connect to Arduino RESET pin
+
+void setup(){
+
+}
+
+void loop(){
+
+}
